1ad80b90af8c1f9bb72a5236f2c88addee57e750
Willy Kuchler <51534875+wdkuchler@users.noreply.github.com>
2021-04-06 12:08:47 -0300

    Add files via upload

--- numato.go
+++ numato.go
@@ -1,12 +1,14 @@
 package main
 
 // para teste -> http://localhost:8080/?device=com6&command=pulse
+// para teste -> http://localhost:8080/?device=/dev/ttyACM0&command=pulse
 
 import (
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"syscall"
 	"time"
 )
 
@@ -14,7 +16,28 @@ const wakeUp = "\r"
 const openIt = "relay on 0\r"
 const closeIt = "relay off 0\r"
 
+func doCommand(w http.ResponseWriter, device string, command string) {
+
+	fmt.Fprintf(w, "Numato - openning device %s\n", device)
+
+	var tty, err = os.OpenFile(device, syscall.O_RDWR|syscall.O_NOCTTY|syscall.O_NONBLOCK, 0666)
+	if err != nil {
+		fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
+		return
+	}
+	defer tty.Close()
+
+	fmt.Fprintf(w, "Numato - writing %s on device %s\n", command, device)
+
+	_, err = tty.WriteString(command)
+	if err != nil {
+		fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
+		return
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+
 	r.ParseForm()
 
 	d := r.Form.Get("device")
@@ -24,53 +47,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Numato - invalid parameters\n")
 		return
 	}
+	doCommand(w, d, wakeUp)
 
-	fmt.Fprintf(w, "Numato - openning device %s\n", d)
-
-	var file, err = os.Create(d)
-	if err != nil {
-		fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
-		return
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(wakeUp)
-	if err != nil {
-		fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
-		return
-	}
 	time.Sleep(50 * time.Millisecond)
-
-	fmt.Fprintf(w, "Numato - writing %s on device %s\n", c, d)
 
 	switch c {
 	case "on":
-		_, err = file.WriteString(openIt)
-		if err != nil {
-			fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
-			return
-		}
-		fmt.Fprintf(w, "wrote %s command on device=%s\n", c, d)
+		doCommand(w, d, openIt)
 	case "off":
-		_, err = file.WriteString(closeIt)
-		if err != nil {
-			fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
-			return
-		}
-		fmt.Fprintf(w, "wrote %s command on device=%s\n", c, d)
+		doCommand(w, d, closeIt)
 	case "pulse":
-		_, err = file.WriteString(openIt)
-		if err != nil {
-			fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
-			return
-		}
+		doCommand(w, d, openIt)
 		time.Sleep(1000 * time.Millisecond)
-		_, err = file.WriteString(closeIt)
-		if err != nil {
-			fmt.Fprintf(w, "Numato - Error %s\n", err.Error())
-			return
-		}
-		fmt.Fprintf(w, "wrote %s command on device=%s\n", c, d)
+		doCommand(w, d, closeIt)
 	default:
 		fmt.Fprintf(w, "invalid command %s\n", c)
 	}
